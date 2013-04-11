b0e7209fd31c35aa58b7232a5b63fdc2077d0371
Erik Englund <erik.englund@axiosengineering.com>
2013-04-11 17:47:48 -0400

    Remove used imports.

--- plugins/mil.jpeojtrs.sca.util/src/mil/jpeojtrs/sca/util/ProtectedThreadExecutor.java
+++ plugins/mil.jpeojtrs.sca.util/src/mil/jpeojtrs/sca/util/ProtectedThreadExecutor.java
@@ -15,10 +15,8 @@ import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
-import java.util.concurrent.ThreadFactory;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.TimeoutException;
-import java.util.concurrent.atomic.AtomicInteger;
 
 /**
  * @since 3.1
