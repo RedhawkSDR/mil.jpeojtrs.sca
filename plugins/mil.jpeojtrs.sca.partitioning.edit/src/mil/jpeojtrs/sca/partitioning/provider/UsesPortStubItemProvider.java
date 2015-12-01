9e2e007046f82c352c104915e1a69ac74317fb7b
Daniel Wille <daniel.wille@axiosengineering.com>
2015-12-01 12:56:52 -0500

    Make label provides for uses/provides port stubs more user-friendly

--- plugins/mil.jpeojtrs.sca.partitioning.edit/src/mil/jpeojtrs/sca/partitioning/provider/UsesPortStubItemProvider.java
+++ plugins/mil.jpeojtrs.sca.partitioning.edit/src/mil/jpeojtrs/sca/partitioning/provider/UsesPortStubItemProvider.java
@@ -176,14 +176,14 @@ public class UsesPortStubItemProvider
 	 * This returns the label text for the adapted class.
 	 * <!-- begin-user-doc -->
 	 * <!-- end-user-doc -->
-	 * @generated
+	 * @generated NOT
 	 */
 	@Override
 	public String getText(Object object) {
 		String label = ((UsesPortStub)object).getName();
 		return label == null || label.length() == 0 ?
 			getString("_UI_UsesPortStub_type") :
-			getString("_UI_UsesPortStub_type") + " " + label;
+			label;
 	}
 
 	/**
