642f2fe2bef8a07a2d30b93d66a601b66a5f7e73
Daniel Wille <daniel.wille@axiosengineering.com>
2016-05-03 11:22:35 -0400

    Correct API version tag

--- plugins/mil.jpeojtrs.sca.util/src/mil/jpeojtrs/sca/util/CFErrorFormatter.java
+++ plugins/mil.jpeojtrs.sca.util/src/mil/jpeojtrs/sca/util/CFErrorFormatter.java
@@ -35,7 +35,7 @@ import CF.ResourcePackage.StopError;
 /**
  * Utility methods for formatting CF exceptions for display to the user. Since they're CORBA exceptions,
  * <code>toString()</code> doesn't produce a message with the error's details.
- * @since 3.5
+ * @since 3.7
  */
 public class CFErrorFormatter {
 
