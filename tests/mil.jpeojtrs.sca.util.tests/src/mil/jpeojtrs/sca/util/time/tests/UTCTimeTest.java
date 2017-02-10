161b456f149592554a24127cced3ce6d24d2aa51
Daniel Wille <DWille@lgsinnovations.com>
2017-02-10 18:28:50 -0500

    IDE-1832 Test UTCTime class

---
+++ tests/mil.jpeojtrs.sca.util.tests/src/mil/jpeojtrs/sca/util/time/tests/UTCTimeTest.java
@@ -0,0 +1,152 @@
+/**
+ * This file is protected by Copyright.
+ * Please refer to the COPYRIGHT file distributed with this source distribution.
+ *
+ * This file is part of REDHAWK IDE.
+ *
+ * All rights reserved.  This program and the accompanying materials are made available under
+ * the terms of the Eclipse Public License v1.0 which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html.
+ */
+package mil.jpeojtrs.sca.util.time.tests;
+
+import org.jacorb.JacorbUtil;
+import org.junit.Assert;
+import org.junit.Test;
+import org.omg.CORBA.Any;
+import org.omg.CORBA.ORB;
+
+import CF.UTCTimeHelper;
+import CF.UTCTimeSequenceHelper;
+import mil.jpeojtrs.sca.util.time.UTCTime;
+
+public class UTCTimeTest {
+
+	private static final ORB ORB = JacorbUtil.init();
+
+	@Test
+	public void new_cfTime() {
+		UTCTime time = new UTCTime(new CF.UTCTime((short) 1, 2.0, 0.3));
+		new_common(time);
+	}
+
+	@Test
+	public void new_shortDoubleDouble() {
+		UTCTime time = new UTCTime((short) 1, 2.0, 0.3);
+		new_common(time);
+	}
+
+	private void new_common(UTCTime time) {
+		Assert.assertEquals(2.0, time.getWholeSeconds(), 0.0);
+		Assert.assertEquals(0.3, time.getFractionalSeconds(), 0.0);
+		Assert.assertEquals((short) 1, time.getStatus());
+		Assert.assertNotNull(time.getWrappedTime());
+		Assert.assertEquals(2.0, time.getWrappedTime().twsec, 0.0);
+		Assert.assertEquals(0.3, time.getWrappedTime().tfsec, 0.0);
+		Assert.assertEquals((short) 1, time.getWrappedTime().tcstatus);
+	}
+
+	@Test
+	public void toAny() {
+
+	}
+
+	@Test
+	public void valueOf_any() {
+		CF.UTCTime cfTime = new CF.UTCTime((short) 1, 2.0, 0.3);
+		Any any = ORB.create_any();
+		UTCTimeHelper.insert(any, cfTime);
+
+		UTCTime time = UTCTime.valueOf(any);
+		Assert.assertEquals(cfTime.twsec, time.getWrappedTime().twsec, 0.0);
+		Assert.assertEquals(cfTime.tfsec, time.getWrappedTime().tfsec, 0.0);
+		Assert.assertEquals(cfTime.tcstatus, time.getWrappedTime().tcstatus);
+	}
+
+	@Test
+	public void valueOfSequence() {
+		CF.UTCTime[] cfTimes = new CF.UTCTime[2];
+		cfTimes[0] = new CF.UTCTime((short) 1, 2.0, 0.3);
+		cfTimes[1] = new CF.UTCTime((short) 0, 4.0, 0.5);
+		Any any = ORB.create_any();
+		UTCTimeSequenceHelper.insert(any, cfTimes);
+
+		UTCTime[] times = UTCTime.valueOfSequence(any);
+		Assert.assertEquals(2, times.length);
+		Assert.assertEquals(2.0, times[0].getWrappedTime().twsec, 0.0);
+		Assert.assertEquals(0.3, times[0].getWrappedTime().tfsec, 0.0);
+		Assert.assertEquals((short) 1, times[0].getWrappedTime().tcstatus);
+		Assert.assertEquals(4.0, times[1].getWrappedTime().twsec, 0.0);
+		Assert.assertEquals(0.5, times[1].getWrappedTime().tfsec, 0.0);
+		Assert.assertEquals((short) 0, times[1].getWrappedTime().tcstatus);
+	}
+
+	@Test
+	public void valueOf_string() {
+		UTCTime time = UTCTime.valueOf("1970:01:01::00:00:00.000000");
+
+		Assert.assertEquals(0.0, time.getWrappedTime().twsec, 0.0);
+		Assert.assertEquals(0.0, time.getWrappedTime().tfsec, 0.0);
+		Assert.assertEquals((short) 1, time.getWrappedTime().tcstatus);
+
+		time = UTCTime.valueOf("1970:01:01::00:00:00");
+		Assert.assertEquals(0.0, time.getWrappedTime().twsec, 0.0);
+		Assert.assertEquals(0.0, time.getWrappedTime().tfsec, 0.0);
+		Assert.assertEquals((short) 1, time.getWrappedTime().tcstatus);
+
+		time = UTCTime.valueOf("2017:02:10::18:02:00.123456");
+
+		Assert.assertEquals(1486749720.0, time.getWrappedTime().twsec, 0.0);
+		Assert.assertEquals(0.123456, time.getWrappedTime().tfsec, 0.0);
+		Assert.assertEquals((short) 1, time.getWrappedTime().tcstatus);
+
+		try {
+			UTCTime.valueOf("1234567890");
+			Assert.fail("Expected an IllegalArgumentException");
+		} catch (IllegalArgumentException e) {
+			// PASS
+		}
+	}
+
+	@Test
+	public void to_String() {
+		UTCTime time = new UTCTime((short) 1, 0.0, 0.0);
+		Assert.assertEquals("1970:01:01::00:00:00", time.toString());
+
+		time = new UTCTime((short) 1, 1486749720.0, 0.123456);
+		Assert.assertEquals("2017:02:10::18:02:00.123456", time.toString());
+	}
+
+	@Test
+	public void compare_To() {
+		UTCTime time1 = new UTCTime((short) 1, 2.0, 0.3);
+		UTCTime time2 = new UTCTime((short) 1, 2.0, 0.4);
+		UTCTime time3 = new UTCTime((short) 1, 3.0, 0.1);
+		UTCTime time4Bad = new UTCTime((short) 0, 2.0, 0.3);
+		Assert.assertEquals(0, time1.compareTo(time1));
+		Assert.assertEquals(-1, time1.compareTo(time2));
+		Assert.assertEquals(-1, time1.compareTo(time3));
+		Assert.assertEquals(1, time1.compareTo(time4Bad));
+		Assert.assertEquals(1, time2.compareTo(time1));
+		Assert.assertEquals(0, time2.compareTo(time2));
+		Assert.assertEquals(-1, time2.compareTo(time3));
+		Assert.assertEquals(1, time2.compareTo(time4Bad));
+		Assert.assertEquals(1, time3.compareTo(time1));
+		Assert.assertEquals(1, time3.compareTo(time2));
+		Assert.assertEquals(0, time3.compareTo(time3));
+		Assert.assertEquals(1, time3.compareTo(time4Bad));
+		Assert.assertEquals(-1, time4Bad.compareTo(time1));
+		Assert.assertEquals(-1, time4Bad.compareTo(time2));
+		Assert.assertEquals(-1, time4Bad.compareTo(time3));
+		Assert.assertEquals(0, time4Bad.compareTo(time4Bad));
+	}
+
+	@Test
+	public void equals_utcTime() {
+		UTCTime time1 = new UTCTime((short) 1, 2.0, 0.3);
+		UTCTime time2 = new UTCTime((short) 1, 4.0, 0.5);
+		UTCTime time3 = new UTCTime((short) 1, 2.0, 0.3);
+		Assert.assertEquals(time1, time3);
+		Assert.assertNotEquals(time1, time2);
+	}
+}
