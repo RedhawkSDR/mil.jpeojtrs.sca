0c18b3c03209351ff7545a4db971888aa51460ac
Devin Nelson <dnelson2@lgsinnovations.com>
2017-08-22 20:38:15 -0400

    IDE-2055 - added icons for devices/services to DcdComponentInstantiationItemProvider

--- plugins/mil.jpeojtrs.sca.dcd.edit/src/mil/jpeojtrs/sca/dcd/provider/DcdComponentInstantiationItemProvider.java
+++ plugins/mil.jpeojtrs.sca.dcd.edit/src/mil/jpeojtrs/sca/dcd/provider/DcdComponentInstantiationItemProvider.java
@@ -13,11 +13,7 @@ package mil.jpeojtrs.sca.dcd.provider;
 
 import java.util.Collection;
 import java.util.List;
-import mil.jpeojtrs.sca.dcd.DcdComponentInstantiation;
-import mil.jpeojtrs.sca.dcd.DcdPackage;
-import mil.jpeojtrs.sca.partitioning.PartitioningFactory;
-import mil.jpeojtrs.sca.partitioning.PartitioningPackage;
-import mil.jpeojtrs.sca.partitioning.provider.ComponentInstantiationItemProvider;
+
 import org.eclipse.emf.common.notify.AdapterFactory;
 import org.eclipse.emf.common.notify.Notification;
 import org.eclipse.emf.common.util.ResourceLocator;
@@ -26,6 +22,15 @@ import org.eclipse.emf.edit.provider.ComposeableAdapterFactory;
 import org.eclipse.emf.edit.provider.IItemPropertyDescriptor;
 import org.eclipse.emf.edit.provider.ViewerNotification;
 
+import mil.jpeojtrs.sca.dcd.DcdComponentInstantiation;
+import mil.jpeojtrs.sca.dcd.DcdPackage;
+import mil.jpeojtrs.sca.partitioning.PartitioningFactory;
+import mil.jpeojtrs.sca.partitioning.PartitioningPackage;
+import mil.jpeojtrs.sca.partitioning.provider.ComponentInstantiationItemProvider;
+import mil.jpeojtrs.sca.scd.SoftwareComponent;
+import mil.jpeojtrs.sca.spd.SpdPackage;
+import mil.jpeojtrs.sca.util.ScaEcoreUtils;
+
 /**
  * This is the item provider adapter for a {@link mil.jpeojtrs.sca.dcd.DcdComponentInstantiation} object.
  * <!-- begin-user-doc -->
@@ -33,6 +38,12 @@ import org.eclipse.emf.edit.provider.ViewerNotification;
  * @generated
  */
 public class DcdComponentInstantiationItemProvider extends ComponentInstantiationItemProvider {
+
+	private static final EStructuralFeature[] COMP_INST_TO_SCD_PATH = new EStructuralFeature[] {
+		PartitioningPackage.Literals.COMPONENT_INSTANTIATION__PLACEMENT, PartitioningPackage.Literals.COMPONENT_PLACEMENT__COMPONENT_FILE_REF,
+		PartitioningPackage.Literals.COMPONENT_FILE_REF__FILE, PartitioningPackage.Literals.COMPONENT_FILE__SOFT_PKG, SpdPackage.Literals.SOFT_PKG__DESCRIPTOR,
+		SpdPackage.Literals.DESCRIPTOR__COMPONENT };
+
 	/**
 	 * This constructs an instance from a factory and a notifier.
 	 * <!-- begin-user-doc -->
@@ -112,6 +123,23 @@ public class DcdComponentInstantiationItemProvider extends ComponentInstantiatio
 	@Override
 	public String getText(Object object) {
 		return super.getText(object);
+	}
+
+	@Override
+	public Object getImage(Object object) {
+		if (object instanceof DcdComponentInstantiation) {
+			DcdComponentInstantiation compInst = (DcdComponentInstantiation) object;
+			SoftwareComponent scd = ScaEcoreUtils.getFeature(compInst, COMP_INST_TO_SCD_PATH);
+			switch (SoftwareComponent.Util.getWellKnownComponentType(scd)) {
+			case DEVICE:
+				return overlayImage(object, getResourceLocator().getImage("full/obj16/Device"));
+			case SERVICE:
+				return overlayImage(object, getResourceLocator().getImage("full/obj16/Service"));
+			default:
+				break;
+			}
+		}
+		return super.getImage(object);
 	}
 
 	/**
