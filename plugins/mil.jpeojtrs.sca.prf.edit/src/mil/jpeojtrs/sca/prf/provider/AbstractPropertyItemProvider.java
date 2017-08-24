e5d27c46e14d8c618848807f7059e04ad9840c4a
Daniel Wille <DWille@lgsinnovations.com>
2017-08-24 15:07:27 -0400

    IDE-2066 Automatically create scoped IDs based on property names

--- plugins/mil.jpeojtrs.sca.prf.edit/src/mil/jpeojtrs/sca/prf/provider/AbstractPropertyItemProvider.java
+++ plugins/mil.jpeojtrs.sca.prf.edit/src/mil/jpeojtrs/sca/prf/provider/AbstractPropertyItemProvider.java
@@ -14,6 +14,8 @@ package mil.jpeojtrs.sca.prf.provider;
 import java.util.Collection;
 import java.util.List;
 
+import org.eclipse.emf.common.command.Command;
+import org.eclipse.emf.common.command.CompoundCommand;
 import org.eclipse.emf.common.notify.AdapterFactory;
 import org.eclipse.emf.common.notify.Notification;
 import org.eclipse.emf.common.util.ResourceLocator;
@@ -22,6 +24,8 @@ import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.EStructuralFeature;
 import org.eclipse.emf.ecore.util.FeatureMap;
 import org.eclipse.emf.ecore.util.FeatureMapUtil;
+import org.eclipse.emf.edit.command.SetCommand;
+import org.eclipse.emf.edit.domain.EditingDomain;
 import org.eclipse.emf.edit.provider.ComposeableAdapterFactory;
 import org.eclipse.emf.edit.provider.IEditingDomainItemProvider;
 import org.eclipse.emf.edit.provider.IItemColorProvider;
@@ -39,6 +43,7 @@ import org.eclipse.emf.edit.provider.ViewerNotification;
 import mil.jpeojtrs.sca.prf.AbstractProperty;
 import mil.jpeojtrs.sca.prf.PrfPackage;
 import mil.jpeojtrs.sca.prf.Properties;
+import mil.jpeojtrs.sca.util.DceUuidUtil;
 
 /**
  * This is the item provider adapter for a {@link mil.jpeojtrs.sca.prf.AbstractProperty} object.
@@ -206,10 +211,8 @@ public class AbstractPropertyItemProvider extends ItemProviderAdapter implements
 		return PrfEditPlugin.INSTANCE;
 	}
 
-	/**
-	 * {@inheritDoc}
-	 * @generated NOT
-	 */
+	// END GENERATED CODE
+
 	@Override
 	public Object getColumnImage(Object object, int columnIndex) {
 		switch (columnIndex) {
@@ -238,4 +241,116 @@ public class AbstractPropertyItemProvider extends ItemProviderAdapter implements
 		return "";
 	}
 
+	/**
+	 * If scoped IDs are created automatically.
+	 */
+	private boolean setIdScoping = false;
+
+	/**
+	 * Changes whether automatic creation of scoped IDs occurs. When enabled, changing a property's name will
+	 * automatically create a scoped ID for it. Likewise, changeing a top-level property's name will cause all
+	 * child elements of the property to have new scoped IDs created. By default scoping is disabled.
+	 * @param enable Whether ID scoping should be enabled
+	 * @since 3.1
+	 */
+	public void setIdScoping(boolean enable) {
+		this.setIdScoping = enable;
+	}
+
+	@Override
+	protected Command createSetCommand(EditingDomain domain, EObject owner, EStructuralFeature feature, Object value, int index) {
+		// Our logic is specialized only if ID scoping is enabled, and the ID or name is being changed
+		if (setIdScoping) {
+			int featureID = feature.getFeatureID();
+			switch (featureID) {
+			case PrfPackage.ABSTRACT_PROPERTY__NAME:
+				return createSetNameCommand(domain, owner, feature, value, index);
+			case PrfPackage.ABSTRACT_PROPERTY__ID:
+				return createSetIDCommand(domain, owner, feature, value, index);
+			default:
+			}
+		}
+
+		return super.createSetCommand(domain, owner, feature, value);
+	}
+
+	private Command createSetIDCommand(EditingDomain domain, EObject owner, EStructuralFeature feature, Object value, int index) {
+		// Our command will include the existing ID change
+		final CompoundCommand command = new CompoundCommand();
+		command.append(super.createSetCommand(domain, owner, feature, value));
+
+		// Only a top-level property affects changes to the IDs of its children
+		if (owner.eContainer() instanceof AbstractProperty) {
+			return command.getCommandList().get(0);
+		}
+
+		// Update all children
+		owner.eAllContents().forEachRemaining(child -> {
+			// Must be a property
+			if (!(child instanceof AbstractProperty)) {
+				return;
+			}
+			AbstractProperty childProp = (AbstractProperty) child;
+
+			// Must have a non-null, non-DCE ID
+			String childID = childProp.getId();
+			if (childID == null || DceUuidUtil.isValid(childID)) {
+				return;
+			}
+
+			// Re-calculate the child property's ID
+			String prefix;
+			if (value == null || DceUuidUtil.isValid((String) value)) {
+				prefix = "";
+			} else {
+				prefix = ((String) value) + "::";
+			}
+			int colonIndex = childID.lastIndexOf("::");
+			String suffix = (colonIndex == -1) ? childID : childID.substring(colonIndex + 2);
+			String newID = prefix + suffix;
+
+			// Update the child's ID if different
+			if (!newID.equals(childID)) {
+				command.append(SetCommand.create(domain, childProp, feature, newID));
+			}
+		});
+
+		// Unwrap in case we didn't add anything
+		return command.unwrap();
+	}
+
+	private Command createSetNameCommand(EditingDomain domain, EObject owner, EStructuralFeature feature, Object value, int index) {
+		// Our command will include the existing name change
+		final CompoundCommand command = new CompoundCommand();
+		command.append(super.createSetCommand(domain, owner, feature, value));
+
+		// We don't change the ID if the new name is null
+		if (value == null) {
+			return command.getCommandList().get(0);
+		}
+
+		// We don't change the ID if the existing ID is a DCE
+		EStructuralFeature idFeature = PrfPackage.eINSTANCE.getAbstractProperty_Id();
+		String id = (String) owner.eGet(idFeature);
+		if (DceUuidUtil.isValid(id)) {
+			return command.getCommandList().get(0);
+		}
+
+		// Compute the prefix
+		EObject topLevelProp = owner;
+		while (topLevelProp.eContainer() instanceof AbstractProperty) {
+			topLevelProp = topLevelProp.eContainer();
+		}
+		String prefix = "";
+		if (owner != topLevelProp) {
+			String topLevelID = ((AbstractProperty) topLevelProp).getId();
+			if (topLevelID != null && !DceUuidUtil.isValid(topLevelID)) {
+				prefix = topLevelID + "::";
+			}
+		}
+
+		// Set the ID
+		command.append(SetCommand.create(domain, owner, idFeature, prefix + value));
+		return command;
+	}
 }
